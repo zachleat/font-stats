687048e9dbbda3c66dcdbe3818351c738301b69d
Zach Leatherman <zachleatherman@gmail.com>
2017-12-25 20:24:43 -0600

    Needn’t be async

--- parse.js
+++ parse.js
@@ -1,6 +1,4 @@
 const FontStatsDir = require("./src/FontStatsDir");
 
-(async function() {
-	let stats = new FontStatsDir("fonts/");
-	console.log( stats.fetchAll() );
-})();
+let stats = new FontStatsDir("fonts/");
+console.log( stats.fetchAll() );
